a794921bcd662b95b37c1145e8e7909eed2cc18b
jihane harimech <jihane.harimech@gmail.com>
2023-10-19 11:15:05 +0200

    user can add an article

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
     #[Route('/', name: 'app_home')]
     public function index(ArticleRepository $articleRepository): Response
     {
@@ -20,5 +25,30 @@ class HomeController extends AbstractController
                 'articles' => $articles,
             ]
         );
+    }
+
+    #[Route('/create', name: 'article_create')]
+    public function create(Request $request, ArticleRepository $articleRepository)
+    {
+        $article = new Article();
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Lier l'utilisateur actuellement connecté comme createur de l'article
+            $article->setUser($this->getUser());
+
+            // Définir la date de publication comme la date actuelle
+            $article->setDatePublication(new \DateTime());
+
+            $articleRepository->save($article, true);
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('home/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
